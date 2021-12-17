1e9a9c5689890bab792d8d60562a32fcb2f656ce
Omer <omerg863@gmail.com>
2021-12-17 08:20:55 +0200

    added email

--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Bio, Title, Skill, Education, Experience, Project
 from django.template.defaulttags import register
+from portfolio.settings import EMAIL_HOST_USER
+from django.core.mail import send_mail
+
 
 def base(request):
     bio = Bio.objects.all().first()
@@ -9,6 +12,16 @@ def base(request):
     educations = Education.objects.all()
     experiences = Experience.objects.all()
     projects = Project.objects.all()
+    if request.method == 'POST':
+        if 'message' in request.POST:
+            message = request.POST.get('message')
+            print(message)
+            name = request.POST.get('message-name')
+            email = request.POST.get('message-email')
+            subject = request.POST.get('message-subject')
+            print(EMAIL_HOST_USER)
+            print(bio.email)
+            send_mail(subject, f'{name}\n{email}\n{message}', EMAIL_HOST_USER, [bio.email], fail_silently=False)
     context = {
         'bio': bio,
         'titles': titles,
@@ -22,6 +35,6 @@ def base(request):
 
 @register.filter
 def num_string(num):
-    if num < 10:
+    if num < 10 and num != 0:
         return "0" + str(num)
     return str(num)
